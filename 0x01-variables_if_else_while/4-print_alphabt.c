b754af66ba655dcadf7f067caf59c749748b32c3
Ardorabelle <ardorabelle2005@gmail.com>
2023-09-15 08:57:46 -0700

    prints all single digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,23 +11,29 @@ int main(void)
 {
 	int i = 1;
 	int digits = (96);
+	/*prints letters before 'e'*/
 	while (digits < 100)
 	{
 		digits = digits + i;
 		putchar(digits);
 	}
+	/*declaration of variable of value 'e'*/
 	digits = (101);
+	/*prints letters after 'e' and before 'q'*/
 	while (digits < 112)
 	{
 		digits = digits + i;
 		putchar(digits);
 	}
+	/*declaration of variable of value 'q'*/
 	digits = (113);
+	/*prints letters after 'q'*/
 	while (digits < 122)
 	{
 		digits = digits + i;
 		putchar(digits);
 	}
+	putchar(10);
 	return (0);
 
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - this is the main function
+ *main -  prints all single digit numbers of base 10 starting from 0, followed by a new line
+ *
+ *Return: 0
+*/
+
+int main(void)
+{
+	/*prints numbers*/
+	printf("0123456789\n");
+	return (0);
+}
