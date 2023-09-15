95fe573a6fc65a51cd50bc2b7d0e16d2997929b2
Ardorabelle <ardorabelle2005@gmail.com>
2023-09-15 08:33:30 -0700

    prints the alphabet in lowercase, and then in uppercase, followed by a new line.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,8 @@
 
 /**
  *main - this is the main function
- * main - this will  prints the alphabet in lowercase, and then in uppercase, followed by a new line
+ *main - this will prints the alphabet in lowercase,and then in uppercase,
+ *followed by a new line
  *
  *Return: 0
 */
@@ -11,9 +12,9 @@ int main(void)
 {
 	/*declaring variable for printing lowercase letters*/
 	int i = 1;
-	int digits =(96);
+	int digits = (96);
 
-	/*conditin to print the lowercase letters using their ascii values*/
+	/*condition to print the lowercase letters using their ascii values*/
 	while (digits < 122)
 	{
 		digits = digits + i;
