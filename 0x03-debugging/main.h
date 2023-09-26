8825f6dd39876ce2e2e3bd07c780db19495b15eb
Ardorabelle <ardorabelle2005@gmail.com>
2023-09-26 13:01:24 -0700

    debugging a program

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,5 +5,7 @@
 
 /*checks if number is positive or negative*/
 int positive_or_negative(int);
+/*returns the largest of three numbers*/
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
